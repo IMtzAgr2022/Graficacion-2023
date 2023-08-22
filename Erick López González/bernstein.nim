f4bc33735c6320ba06b136df35400b688a072012
Erick López González <115573241+lerick80@users.noreply.github.com>
2023-08-21 23:35:41 -0600

    Add files via upload

---
+++ Erick López González/bernstein.nim
@@ -0,0 +1,22 @@
+# Algoritmo 1.2 Polinomio de Bernstein
+# Erick López González
+
+proc Bernstein(i, n: int, u: float): float =
+  var temp: seq[float] = newSeq[float](n + 1)
+
+  for j in 0..n:
+    temp[j] = 0.0
+
+  temp[n - i] = 1.0
+  for k in 1..n:
+    for j in n-1 .. k-1:
+            temp[j] = float(i) * temp[j] + u * temp[j + 1]
+    return temp[n]
+
+# EJEMPLO
+when isMainModule:
+  let i = 3
+  let n = 6
+  let u = 0.2
+  let result = Bernstein(i, n, u)
+  echo "El resultado del polinomio de Bernstein es: ", result
