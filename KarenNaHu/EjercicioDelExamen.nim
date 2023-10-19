da5b62225dec54429544fcaf68917d124ff7897c
Karen Navarro Hurtado <101692582+karennahu@users.noreply.github.com>
2023-10-19 11:22:09 -0800

    Create EjercicioDelExamen.nim

---
+++ KarenNaHu/EjercicioDelExamen.nim
@@ -0,0 +1,4 @@
+       Tip: 1 messages have been suppressed, use --verbose to show them.
+packageinfo.nim(274)     findNimbleFile
+
+    Error:  Could not find a file with a .nimble extension inside the specified directory: C:\Users\knava\Downloads\miProyectoNim
